bfe4602951803a825a034dc119bbc72a51d40eef
rare <rarenftclub@gmail.com>
2023-06-15 17:19:30 -0700

    design update

--- src/components/Web3Connect/index.tsx
+++ src/components/Web3Connect/index.tsx
@@ -38,15 +38,34 @@ export default function Web3Connect({ color = 'gray', size = 'sm', className = '
   };
 
   return error ? (
-    <div
-      className="flex items-center justify-center px-4 py-2 font-semibold text-white border rounded bg-opacity-80 border-red bg-red hover:bg-opacity-100"
-      onClick={toggleWalletModal}
-    >
-      <div className="mr-1">
-        <Activity className="w-4 h-4" />
-      </div>
-      {error instanceof UnsupportedChainIdError ? i18n._(t`Wrong Network!`) : i18n._(t`Error`)}
-    </div>
+    <SoundsProvider sounds={createSounds(sounds)}>
+
+      <Frame
+        animate={true}
+        level={3}
+        corners={4}
+        layer='alert'
+        onClick={toggleWalletModal}
+        variant="outlined"
+        color={color}
+        className="py-2 mx-auto text-center bg-transparent w-max"
+        size={size}
+        {...rest}
+      >
+        <div
+          className="flex items-center justify-center px-1 py-0 font-semibold text-white bg-opacity-75 border-red bg-red hover:bg-opacity-100"
+          onClick={toggleWalletModal}
+        >
+          <div className="mr-1">
+            <Activity className="w-4 h-4" />
+          </div>
+          <Player sfx1="click" sfx2="ask" content={i18n._(t`Wrong Network!`)}>
+            {error instanceof UnsupportedChainIdError ? i18n._(t`Wrong Network!`) : i18n._(t`Error`)}
+          </Player>
+        </div>
+      </Frame>
+    </SoundsProvider>
+
   ) : (
     <SoundsProvider sounds={createSounds(sounds)}>
       <Frame
