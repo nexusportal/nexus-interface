7d2f010feb9a38919b7a742493940099b4d3b99e
rare <rarenftclub@gmail.com>
2024-07-30 19:19:44 -0700

    fix farm reward icon

--- src/features/onsen/FarmListItem.tsx
+++ src/features/onsen/FarmListItem.tsx
@@ -21,30 +21,29 @@ const FarmListItem: FC<FarmListItem> = ({ farm, onClick }) => {
   const token0 = useCurrency(farm.pair.token0?.id) ?? undefined
   const token1 = useCurrency(farm.pair.token1?.id) ?? undefined
 
+  // Calculate NEXU allocation percentage
+  const nexuAllocationPercentage = (farm.allocPoint * 100 / farm.owner.totalAllocPoint).toFixed(1)
+
   return (
     <div className={classNames(TABLE_TBODY_TR_CLASSNAME, 'grid grid-cols-6')} onClick={onClick}>
-      <div className={classNames(`flex ${token0 === token1?"gap-7":"gap-2"}`, TABLE_TBODY_TD_CLASSNAME(0, 6))}>
+      <div className={classNames(`flex ${token0 === token1 ? "gap-7" : "gap-2"}`, TABLE_TBODY_TD_CLASSNAME(0, 6))}>
         {token0 && token1 && (token0.symbol !== token1.symbol) && <CurrencyLogoArray currencies={[token0, token1]} dense size={32} className='max-w-none' />}
-
-        {(token0 && !token1 || token0?.symbol === token1?.symbol) && <CurrencyLogo currency={token0} size={32}  className='max-w-none'  />}
-
+        {(token0 && !token1 || token0?.symbol === token1?.symbol) && <CurrencyLogo currency={token0} size={32} className='max-w-none' />}
         <div className="flex flex-col items-start">
-          {
-            farm?.pair?.type === PairType.SINGLE?(
-              <Typography weight={700} className="flex gap-1 text-high-emphesis">
-            {farm?.pair?.token0?.symbol}
-          </Typography>
-            ):(
-              <Typography weight={700} className="flex gap-1 text-high-emphesis">
-            {farm?.pair?.token0?.symbol}
-            {farm?.pair?.token1 && <span className="text-low-emphesis">/</span>}
-            {farm?.pair?.token1?.symbol}
-          </Typography>
-            )
-          }
+          {farm?.pair?.type === PairType.SINGLE ? (
+            <Typography weight={700} className="flex gap-1 text-high-emphesis">
+              {farm?.pair?.token0?.symbol}
+            </Typography>
+          ) : (
+            <Typography weight={700} className="flex gap-1 text-high-emphesis">
+              {farm?.pair?.token0?.symbol}
+              {farm?.pair?.token1 && <span className="text-low-emphesis">/</span>}
+              {farm?.pair?.token1?.symbol}
+            </Typography>
+          )}
           {farm?.pair?.type === PairType.SWAP && (
             <Typography variant="xs" className="text-low-emphesis">
-              {/* {i18n._(t`Nexus Generator`)} */}
+              {i18n._(t`Nexus Generator`)}
             </Typography>
           )}
           {farm?.pair?.type === PairType.KASHI && (
@@ -62,41 +61,44 @@ const FarmListItem: FC<FarmListItem> = ({ farm, onClick }) => {
       <div className={classNames('flex !items-end justify-end -space-x-1', TABLE_TBODY_TD_CLASSNAME(2, 6))}>
         {/* @ts-ignore TYPE NEEDS FIXING */}
         {farm?.rewards.toReversed()?.map((reward, i) => (
-          <Typography
-            variant="sm"
-            weight={700}
-            key={i}
-            className="flex gap-1.5 text-high-emphesis justify-center items-center"
-            component="span"
-          >
-            {/* {formatNumber(reward.rewardPerDay)} */}
-            <CurrencyLogo currency={reward.currency} size={32} />
-          </Typography>
+          (reward.currency.symbol !== "NEXU" || parseFloat(nexuAllocationPercentage) > 0) && (
+            <Typography
+              variant="sm"
+              weight={700}
+              key={i}
+              className="flex gap-1.5 text-high-emphesis justify-center items-center"
+              component="span"
+            >
+              <CurrencyLogo currency={reward.currency} size={32} />
+            </Typography>
+          )
         ))}
       </div>
       <div className={classNames('flex !items-end justify-end -space-x-1', TABLE_TBODY_TD_CLASSNAME(3, 6))}>
         {/* @ts-ignore TYPE NEEDS FIXING */}
         {farm?.rewards.toReversed()?.map((reward, i) => {
-          if (reward.currency.symbol !== "NEXU") return null;
-          return <Typography
-            variant="sm"
-            weight={700}
-            key={i}
-            className="flex gap-1.5 text-high-emphesis justify-center items-center"
-            component="span"
-          >
-            {formatNumber(reward.rewardPerDay)}
-          </Typography>
+          if (reward.currency.symbol !== "NEXU" || parseFloat(nexuAllocationPercentage) === 0) return null;
+          return (
+            <Typography
+              variant="sm"
+              weight={700}
+              key={i}
+              className="flex gap-1.5 text-high-emphesis justify-center items-center"
+              component="span"
+            >
+              {formatNumber(reward.rewardPerDay)}
+            </Typography>
+          )
         })}
       </div>
       <div className={TABLE_TBODY_TD_CLASSNAME(4, 6)}>
-        <Typography weight={700} variant="sm" className="text-high-emphesis  gap-0.5 items-center pr-4" component="span">
+        <Typography weight={700} variant="sm" className="text-high-emphesis gap-0.5 items-center pr-4" component="span">
           {formatNumber(farm.rewards[0].rewardPerBlock.toFixed(4), false)}
         </Typography>
       </div>
       <div className={TABLE_TBODY_TD_CLASSNAME(5, 6)}>
-        <Typography weight={700} variant="sm" className="text-high-emphesis  gap-0.5 items-center pr-1" component="span">
-          {(farm.allocPoint * 100/farm.owner.totalAllocPoint).toFixed(1)}%
+        <Typography weight={700} variant="sm" className="text-high-emphesis gap-0.5 items-center pr-1" component="span">
+          {(farm.allocPoint * 100 / farm.owner.totalAllocPoint).toFixed(1)}%
         </Typography>
       </div>
       {/* <div className={classNames('flex flex-col !items-end', TABLE_TBODY_TD_CLASSNAME(3, 4))}>
