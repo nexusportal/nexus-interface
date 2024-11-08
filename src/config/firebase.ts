8fc2a1285a075659a51dbfcabcccf4c3597cf9b2
rare <rarenftclub@gmail.com>
2024-11-08 00:21:48 -0800

    update firebase env setup

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore, addDoc, collection } from 'firebase/firestore'
 import { TokenMetadata } from '../hooks/useLauncher'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAk1elbdJ3g2LraxWAY_zyD_RCQlFp0OcU",
-  authDomain: "nexus-launcher-e6b4d.firebaseapp.com",
-  databaseURL: "https://nexus-launcher-e6b4d-default-rtdb.firebaseio.com",
-  projectId: "nexus-launcher-e6b4d",
-  storageBucket: "nexus-launcher-e6b4d.appspot.com",
-  messagingSenderId: "945363227757",
-  appId: "1:945363227757:web:fdae08f289fdc4c9329b64"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
 console.log('🔥 Initializing Firebase...')
